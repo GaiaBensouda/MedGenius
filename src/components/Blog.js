fb5a9df395fa7e0ed93dd010c0375913e3248dcb
GaiaBensouda <gaia.bensouda-korachi@polytechnique.edu>
2024-07-17 12:51:29 +0200

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -6,7 +6,7 @@ const Blog = () => {
     <div className="blog">
       <h2 className="animated-text">Blog</h2>
       <div className="blog-content">
-        {/* Add your blog content here */}
+        {/* blog content */}
         <p>This is where your blog content will go. You can add articles, updates, and other relevant information.</p>
       </div>
     </div>
